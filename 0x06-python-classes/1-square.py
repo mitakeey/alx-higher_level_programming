82921b034a0ff524b9ee0bc382a3b9b422343a4b
mitakeey <mitakeeykiage@gmail.com>
2021-09-28 17:21:32 +0300

    🚧 (initial): class square
    initialize class with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class and initialise it
+"""
+
+
+class Square:
+    """
+Square implementation
+    """
+    def __init__(self, size):
+        self.__size = size
+        
