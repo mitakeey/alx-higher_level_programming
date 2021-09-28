befd915583c681bfa87ce716fc522931cff27b9b
mitakeey <mitakeeykiage@gmail.com>
2021-09-28 17:15:33 +0300

    ➕ (initial): create class square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module defining class square
+"""
+
+
+class Square:
+    """ 
+     class square randomly generates 1 integer to denote one side of a square and store it in the class
+    """
+    pass
