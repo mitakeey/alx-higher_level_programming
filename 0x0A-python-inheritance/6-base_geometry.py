b867040aad51a3e664efc8dd0084c8567cf8ab7f
mitakeey <mitakeeykiage@gmail.com>
2021-10-19 21:39:57 +0300

    python inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,7 +5,11 @@ This module implements an object skeleton for BaseGeometry
 
 
 class BaseGeometry:
-    
+    """implementation
+    """
     def area(self):
-        
+        """find area
+        Raises:
+            Exception: area not implemented
+        """
         raise Exception("area() is not implemented")
