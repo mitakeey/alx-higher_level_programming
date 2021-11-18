02dc33ca0d64a053485d83b954f37f583be08399
mitakeey <mitakeeykiage@gmail.com>
2021-11-18 17:30:46 +0300

    :card_file_box: creating table with descriptions

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,8 @@
--- command to create a table with id and name
-CREATE TABLE first_table(id INT),name VARCHAR(256);
+-- command to create a table with the follwing
+-- first_table description:
+-- id INT
+-- name VARCHAR(256)
+-- The database name will be passed as an argument of the mysql command
+
+If the table first_table already exists, your script should not fail
+CREATE TABLE IF NOT EXISTS first_table(id INT),name VARCHAR(256));
