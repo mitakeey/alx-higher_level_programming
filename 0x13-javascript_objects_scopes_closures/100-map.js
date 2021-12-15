2bee2da9c50028f013905c4e1f16157d97b733ad
mitakeey <mitakeeykiage@gmail.com>
2021-12-15 10:16:36 +0300

    :zap: standard --fix
    javascript

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let list = require('./100-data.js').list;
-console.log(list);
-let newList = list.map((x, index) => x * index);
-console.log(newList);
+const list = require('./100-data.js').list
+console.log(list)
+const newList = list.map((x, index) => x * index)
+console.log(newList)
