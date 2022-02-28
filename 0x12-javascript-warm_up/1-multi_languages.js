b9866cfe7b7a6b2d0f6934230be427aaddeb977b
mitakeey <mitakeeykiage@gmail.com>
2022-02-28 18:55:20 +0300

    :zap: javascript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+/// the following code works with ES6
+/*
+let multiLines = str => {
+    let string = `
+    C is fun
+    Python is cool
+    JavaScript is amazing
+    `
+console.log(multiLines(string));
+}
+*/
+
+console.log("C is fun\nPython is cool\nJavaScript is amazing");
